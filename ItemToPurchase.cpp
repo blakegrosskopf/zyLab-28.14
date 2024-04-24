f8d824a48d8f20725fe02586e48bf09debd9f506
blakegrosskopf <167143507+blakegrosskopf@users.noreply.github.com>
2024-04-24 13:31:05 -0500

    Update ItemToPurchase.cpp
    Final creation and instantiation of the ItemToPurchase class

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,26 @@
+#include "ItemToPurchase.h"
+#include <iostream>
+using namespace std;
 
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity){
+  itemName = name;
+  itemDescription = description;
+  itemPrice = price;
+  itemQuantity = quantity;
+}
+
+void ItemToPurchase::SetDescription(string description){
+  itemDescription = description;
+}
+
+string ItemToPurchase::GetDescription() const{
+  return itemDescription;
+}
+
+void ItemToPurchase::PrintItemCost() const{
+  court<<itemName<<" "<<itemQuanttiy<<" @$"<<itemPrice<<" =$"<<(itemQuantity * itemPrice)<<endl;
+}
+
+void ItemToPurchase::PrintItemDescription() const{
+  court<<itemName<<": " << itemDescription <<endl;
+}
