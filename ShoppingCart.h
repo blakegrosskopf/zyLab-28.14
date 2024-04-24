aa680bf94ee4ac135ba25af07489d2f07808eed0
SajenPatel <167539079+SajenPatel@users.noreply.github.com>
2024-04-24 13:44:08 -0500

    Update ShoppingCart.h

--- ShoppingCart.h
+++ ShoppingCart.h
@@ -1,1 +1,27 @@
+#ifndef SHOPPINGCART_H
+#define SHOPPINGCART_H
 
+#include <string>
+#include <vector>
+#include "ItemToPurchase.h"
+using namespace std;
+
+class ShoppingCart {
+public:
+    ShoppingCart(string customerName = "none", string currentDate = "January 1, 2016");
+    string GetCustomerName() const;
+    string GetDate() const;
+    void AddItem(const ItemToPurchase& item);
+    void RemoveItem(string itemName);
+    void ModifyItem(const ItemToPurchase& item);
+    int GetNumItemsInCart() const;
+    int GetCostOfCart() const;
+    void PrintTotal() const;
+    void PrintDescriptions() const;
+private:
+    string customerName;
+    string currentDate;
+    vector<ItemToPurchase> cartItems;
+};
+
+#endif
