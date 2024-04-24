24f658696497bdb6297ee74d82fdbefcb19cc5aa
blakegrosskopf <167143507+blakegrosskopf@users.noreply.github.com>
2024-04-24 13:43:11 -0500

    Update main.cpp
    Creaiton of ExecuteMenue

--- main.cpp
+++ main.cpp
@@ -10,8 +10,61 @@ void PrintMenu() {
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
-   
+   switch (option) {
+     case 'a': {
+         string name, description;
+         int price, quantity;
+         cout << "ADD ITEM TO CART" << endl;
+         cout << "Enter the item name: ";
+         getline(cin >> ws, name);
+         cout << "Enter the item description: ";
+         getline(cin >> ws, description);
+         cout << "Enter the item price: ";
+         cin >> price;
+         cout << "Enter the item quantity: ";
+         cin >> quantity;
+         ItemToPurchase newItem(name, description, price, quantity);
+         theCart.AddItem(newItem);
+         break;
+     }
+     case 'd': {
+         string name;
+         cout << "REMOVE ITEM FROM CART" << endl;
+         cout << "Enter name of item to remove: ";
+         getline(cin >> ws, name);
+         theCart.RemoveItem(name);
+         break;
+     }
+     case 'c': {
+         string name;
+         int newQuantity;
+         cout << "CHANGE ITEM QUANTITY" << endl;
+         cout << "Enter the item name: ";
+         getline(cin >> ws, name);
+         cout << "Enter the new quantity: ";
+         cin >> newQuantity;
+         ItemToPurchase modifiedItem(name, "", 0, newQuantity);
+         theCart.ModifyItem(modifiedItem);
+         break;
+     }
+     case 'i': {
+         theCart.PrintDescriptions();
+         break;
+     }
+     case 'o': {
+         theCart.PrintTotal();
+         break;
+     }
+     case 'q': {
+         // Quit
+         cout << "Exiting program." << endl;
+         break;
+     }
+     default: {
+         cout << "Invalid option! Please select from the menu." << endl;
+         break;
+     }
+   }
 }
 
 int main() {
