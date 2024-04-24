89d240bc2bac29b2fa8bddde2f862c5e467e0f00
blakegrosskopf <167143507+blakegrosskopf@users.noreply.github.com>
2024-04-24 13:25:38 -0500

    Update ItemToPurchase.h
    Creation of ItemToPurchase class

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,1 +1,20 @@
+#ifndef ITEMTOPURCHASE_H
+#define ITEMTOPURCHASE_H
 
+#include <string>
+using namespace std;
+
+class ItemToPurchase{
+  public:
+    ItemToPurchase(string name = "none", string description = "none", int price = 0, int quantity = 0);
+    void SetDescription(string description);
+    string GetDescription() const;
+    void PrintItemCost() const;
+    void PrintItemDescription() const;
+  private:
+    string itemName;
+    string itemDescription;
+    int itemPrice;
+    int itemQuantity;
+};
+#endif
