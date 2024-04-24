75444a038bd36c1ccf81c950e343f68fa42de55e
blakegrosskopf <167143507+blakegrosskopf@users.noreply.github.com>
2024-04-24 15:10:51 -0500

    Update ItemToPurchase.h
    Adding two methods and fixing name to sync with Sajen's variable names

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -8,9 +8,12 @@ class ItemToPurchase{
   public:
     ItemToPurchase(string name = "none", string description = "none", int price = 0, int quantity = 0);
     void SetDescription(string description);
-    string GetDescription() const;
+    string GetItemDescription() const;
+    int GetQuantity() const;
+    int GetPrice() const;
     void PrintItemCost() const;
     void PrintItemDescription() const;
+    
   private:
     string itemName;
     string itemDescription;
