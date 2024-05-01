d1f8ae22fcbb4870226203ac84b84e1dc9fcd831
SajenPatel <167539079+SajenPatel@users.noreply.github.com>
2024-04-30 19:18:06 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,38 +1,47 @@
 #include "ItemToPurchase.h"
 #include <iostream>
 using namespace std;
+ItemToPurchase::ItemToPurchase() : itemName("none"), itemDescription("none"), itemPrice(0), itemQuantity(0) {}
 
-ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity){
-  itemName = name;
-  itemDescription = description;
-  itemPrice = price;
-  itemQuantity = quantity;
-}
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity)
+    : itemName(name), itemDescription(description), itemPrice(price), itemQuantity(quantity) {}
 
-void ItemToPurchase::SetDescription(string description){
-  itemDescription = description;
-}
-void ItemToPurchase::SetPrice(int price){
-  itemPrice = price;
-}
-void ItemToPurchase::SetQuantity(int itemQuantity){
-  itemQuantity = quantity;
+void ItemToPurchase::SetName(string name) {
+    itemName = name;
 }
 
-string ItemToPurchase::GetDescription() const{
-  return itemDescription;
-}
-int ItemToPurchase::GetPrice() const{
-  return itemPrice;
-}
-int ItemToPurchase::GetQuantity() const{
-  return itemQuantity;
+void ItemToPurchase::SetDescription(string description) {
+    itemDescription = description;
 }
 
-void ItemToPurchase::PrintItemCost() const{
-  cout<<itemName<<" "<<itemQuantity<<" @$"<<itemPrice<<" =$"<<(itemQuantity * itemPrice)<<endl;
+void ItemToPurchase::SetPrice(int price) {
+    itemPrice = price;
 }
 
-void ItemToPurchase::PrintItemDescription() const{
-  cout<<itemName<<": " << itemDescription <<endl;
+void ItemToPurchase::SetQuantity(int quantity) {
+    itemQuantity = quantity;
+}
+
+string ItemToPurchase::GetName() const {
+    return itemName;
+}
+
+string ItemToPurchase::GetDescription() const {
+    return itemDescription;
+}
+
+int ItemToPurchase::GetPrice() const {
+    return itemPrice;
+}
+
+int ItemToPurchase::GetQuantity() const {
+    return itemQuantity;
+}
+
+void ItemToPurchase::PrintItemCost() const {
+    cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " = $" << (itemPrice * itemQuantity) << endl;
+}
+
+void ItemToPurchase::PrintItemDescription() const {
+    cout << itemName << ": " << itemDescription << endl;
 }
